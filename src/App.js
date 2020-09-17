0ea2454b0fc4bf8d4e58d37f1cbdbb21fcd817e4
koniecznytomek <tomek@tomekkonieczny.com>
2020-09-17 11:42:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.style.css';
 import Sample from './components/Sample/Sample';
-import Grids from './components/Grids/Grids';
 
 const App = () => {
   return (
     <>
-      <Grids/>
       <Sample />
     </>
   );
